81f3aef4395aa93ce4da746e9eb310f669892b5a
SuperPony <superponyyy@gmail.com>
2022-01-20 23:09:49 +0800

    feat(pkg/validator): validator pkg done.

---
+++ pkg/validator/options.go
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2021 Kristian Huang <kristianhuang@gmail.com>. All rights reserved.
+ * Use of this source code is governed by a MIT style
+ * license that can be found in the LICENSE file.
+ */
+
+package validator
+
+import "github.com/spf13/pflag"
+
+const (
+	flagLanguage = "validator.language" // 验证器使用的语言 zh, en...
+	flagTag      = "validator.tag"
+)
+
+type Options struct {
+	Language string `json:"language" mapstructure:"language"`
+	// Struct field tag
+	Tag string `json:"tag" mapstructure:"tag"`
+}
+
+func NewOptions() *Options {
+	return &Options{
+		Language: "zh",
+		Tag:      "label",
+	}
+}
+
+func (o *Options) AddFlags(fs *pflag.FlagSet) {
+	fs.StringVar(&o.Language, flagLanguage, o.Language, "Validator use language.")
+	fs.StringVar(&o.Tag, flagTag, o.Tag, "Validator struct field tag.")
+}
---
+++ pkg/validator/types.go
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2021 Kristian Huang <kristianhuang@gmail.com>. All rights reserved.
+ * Use of this source code is governed by a MIT style
+ * license that can be found in the LICENSE file.
+ */
+
+package validator
+
+import (
+	"context"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type Validator interface {
+	Struct(data interface{}) error
+	StructCtx(ctx context.Context, data interface{}) error
+	Var(f interface{}, rule string) error
+	VarCtx(ctx context.Context, f interface{}, rule string) error
+	WithTranslator(language string) Validator
+	GetValidate() *validator.Validate
+	RegisterValidation(tag, errMSg string, vf Validation) error
+}
+
+// Validation custom validation func.
+type Validation func(fl validator.FieldLevel) bool
